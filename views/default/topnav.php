1ec04cd8d257c969c3c715b8bf9fc4bed561c7af
kei113 <90322530+kei113@users.noreply.github.com>
2026-02-19 21:23:35 +0700

    Add files via upload

---
+++ views/default/topnav.php
@@ -0,0 +1,9 @@
+<header class="top-navbar">
+<div class="navbar-left">
+<h1 class="page-title"><?php echo ucwords($_SESSION['username'] ?? 'User'); ?> Dashboard</h1>
+</div>
+<div class="navbar-right">
+<div class="user-dropdown">
+<img src="https://picsum.photos/seed/user123/40/40.jpg" alt="User Avatar" class="user-avatar">
+</div>
+</header>
